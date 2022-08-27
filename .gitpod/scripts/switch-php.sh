7cb7622269240c2f871a4dffc1996ec595bf4e5d
ochorocho <jochen.roth@b13.com>
2022-08-27 12:43:06 +0200

    [TASK] Simplify php switcher

--- .gitpod/scripts/switch-php.sh
+++ .gitpod/scripts/switch-php.sh
@@ -21,16 +21,15 @@ sudo update-alternatives --set php $(which php${PHP})
 for version in $ALL_VERSIONS
 do
     echo "Disable modules for PHP $version ..."
-    # sudo a2dismod mpm_event
-    # sudo a2dismod mpm_prefork
+    sudo a2dismod mpm_prefork
+    sudo a2dismod mpm_event
+    sudo a2dismod mpm_worker
     sudo a2dismod "php$version"
 done
 
-sudo service apache2 restart
+# sudo service apache2 restart
 
 sudo apt install -y -qq libapache2-mod-php$PHP
-sudo a2enmod "php$PHP"
-sudo service apache2 restart
 
 # if [ "$PHP" = "8.1" ]; then
 #     sudo a2enmod "php8.1"
@@ -63,3 +62,6 @@ sudo service apache2 restart
 # if [ "$PHP" = "5.6" ]; then
 #     sudo a2enmod "php5.6"
 # fi
+
+sudo a2enmod "php$PHP"
+sudo service apache2 restart
