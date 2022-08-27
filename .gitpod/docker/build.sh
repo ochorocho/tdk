6e1b468e8587e5029c9cc5c5c4c296a50b62f50b
ochorocho <jochen.roth@b13.com>
2022-08-27 16:08:28 +0200

    [TASK] Use custom docker file

--- .gitpod/docker/build.sh
+++ .gitpod/docker/build.sh
@@ -2,3 +2,5 @@
 
 docker build --no-cache --progress=plain . -f Dockerfile -t ochorocho/gitpod-tdk:latest && docker run --rm -it -v `pwd`/test.sh:/tmp/test.sh --entrypoint "bash" ochorocho/gitpod-tdk:latest /tmp/test.sh
 docker images | grep ochorocho/gitpod-tdk
+
+# docker push ochorocho/gitpod-tdk:latest
