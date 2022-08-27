85fee93a5c9927299138b43f82cc4e7a5eff6e90
ochorocho <jochen.roth@b13.com>
2022-08-27 20:02:20 +0200

    [TASK] Add mailhog init script

--- .gitpod/docker/test.sh
+++ .gitpod/docker/test.sh
@@ -5,3 +5,9 @@
 #php8.0 --version | grep "^PHP" || exit 1
 #php8.1 --version | grep "^PHP" || exit 1
 #apt list --installed | grep 'libapache2-mod-php'
+
+which ~/go/bin/MailHog
+which MailHog || exit 1
+sudo service mailhog start
+sudo service mailhog status
+sudo service mailhog stop
