f56fed11979cc090dae286ed5c444afed13b0bfc
ochorocho <jochen.roth@b13.com>
2022-08-28 14:45:09 +0200

    [TASK] Optimize image, add more details to commands

--- .gitpod/docker/test.sh
+++ .gitpod/docker/test.sh
@@ -9,6 +9,10 @@ php8.0 --version | grep "^PHP" || exit 1
 php8.1 --version | grep "^PHP" || exit 1
 
 # See required modules
+php7.4 -m | grep "curl" || exit 1
+php7.4 -m | grep "zip" || exit 1
+php8.0 -m | grep "curl" || exit 1
+php8.0 -m | grep "zip" || exit 1
 php8.1 -m | grep "curl" || exit 1
 php8.1 -m | grep "zip" || exit 1
 
