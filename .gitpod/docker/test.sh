e2790b40c6cef06937a89fab77aad4d28a2b4214
ochorocho <jochen.roth@b13.com>
2022-08-28 13:43:51 +0200

    [TASK] Move stuff to docker build

--- .gitpod/docker/test.sh
+++ .gitpod/docker/test.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-#composer --version || exit 1
-#php --version | grep "^PHP" || exit 1
-#php8.0 --version | grep "^PHP" || exit 1
-#php8.1 --version | grep "^PHP" || exit 1
-#apt list --installed | grep 'libapache2-mod-php'
-
-which ~/go/bin/MailHog
+composer --version || exit 1
+php --version | grep "^PHP" || exit 1
+php8.0 --version | grep "^PHP" || exit 1
+php8.1 --version | grep "^PHP" || exit 1
 which MailHog || exit 1
-sudo service mailhog start
-sudo service mailhog status
-sudo service mailhog stop
-sudo service mailhog status
