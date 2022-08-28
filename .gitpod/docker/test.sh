570ba84629bc7fae12c1a31ed25ab5dcc2d7aea2
ochorocho <jochen.roth@b13.com>
2022-08-28 14:22:27 +0200

    [BUGFIX] Install required modules

--- .gitpod/docker/test.sh
+++ .gitpod/docker/test.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 composer --version || exit 1
+
+# See php versions
 php --version | grep "^PHP" || exit 1
+php7.4 --version | grep "^PHP" || exit 1
 php8.0 --version | grep "^PHP" || exit 1
 php8.1 --version | grep "^PHP" || exit 1
+
+# See existing modules
+php8.1 -m | grep "curl" || exit 1
+php8.1 -m | grep "zip" || exit 1
+
 which MailHog || exit 1
+
