78eaeac62acd7bd849758b639af14bd058a412e7
ochorocho <jochen.roth@b13.com>
2022-08-29 23:08:08 +0200

    [BUGFIX] Create log file for xdebug

--- .gitpod/docker/test.sh
+++ .gitpod/docker/test.sh
@@ -15,7 +15,7 @@ php8.0 -m | grep "curl" || exit 1
 php8.0 -m | grep "zip" || exit 1
 php8.1 -m | grep "curl" || exit 1
 php8.1 -m | grep "zip" || exit 1
-
+sudo ls -la /var/log/apache2/
 which MailHog || exit 1
 convert --version | grep "^Version" || exit 1
 identify --version | grep "^Version" || exit 1
