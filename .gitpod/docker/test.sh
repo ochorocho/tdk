4674180c91048583d7444b4f665c8e8bff6b2ef3
ochorocho <rothjochen@gmail.com>
2022-09-02 02:13:58 +0200

    [TASK] fix permissions for sqltools cache folder, optimize image size

--- .gitpod/docker/test.sh
+++ .gitpod/docker/test.sh
@@ -2,19 +2,16 @@
 
 composer --version || exit 1
 
-# See php versions
-php --version | grep "^PHP" || exit 1
-php7.4 --version | grep "^PHP" || exit 1
-php8.0 --version | grep "^PHP" || exit 1
-php8.1 --version | grep "^PHP" || exit 1
+# See php and required modules
+ALL_VERSIONS="7.2 7.3 7.4 8.0 8.1"
 
-# See required modules
-php7.4 -m | grep "curl" || exit 1
-php7.4 -m | grep "zip" || exit 1
-php8.0 -m | grep "curl" || exit 1
-php8.0 -m | grep "zip" || exit 1
-php8.1 -m | grep "curl" || exit 1
-php8.1 -m | grep "zip" || exit 1
-which MailHog || exit 1
-convert --version | grep "^Version" || exit 1
-identify --version | grep "^Version" || exit 1
+for version in $ALL_VERSIONS
+do
+    "php$version" -v | grep "^PHP" || (echo "PHP curl 7.4 missing" && exit 1)
+    "php$version" -m | grep "curl" || (echo "PHP curl 7.4 missing" && exit 1)
+    "php$version" -m | grep "zip" || (echo "PHP zip 7.4 missing" && exit 1)
+done
+
+which /home/gitpod/go/bin/MailHog || (echo "MailHog Missing" && exit 1)
+convert --version | grep "^Version" || (echo "convert missing" && exit 1)
+identify --version | grep "^Version" || (echo "identify issing" && exit 1)
