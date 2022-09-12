cc6e8af912ce81a0e5ac06533d449d349c31ab94
ochorocho <jochen.roth@b13.com>
2022-09-13 00:58:02 +0200

    [TASK] Add `tdk ssh-add` command
    GitPod variables do not support multiline.
    Based on the variable SSH_PRIVATE_KEY the `ssh-add` command will take
    the private key, format it as needed will verify and store it.

--- .gitpod/docker/test.sh
+++ .gitpod/docker/test.sh
@@ -2,6 +2,8 @@
 
 composer --version || exit 1
 
+export SSH_PRIVATE_KEY="-----BEGIN OPENSSH PRIVATE KEY----- b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW QyNTUxOQAAACDEMsv3oItZjfGpC37kIksDqV7awvGEhshuQKR4/CjhywAAAJBrwgvIa8IL yAAAAAtzc2gtZWQyNTUxOQAAACDEMsv3oItZjfGpC37kIksDqV7awvGEhshuQKR4/Cjhyw AAAEB3Cdx6Qg76bDTsdi2pdcAyFyeYE9KXRJwvPEW0hjcxJMQyy/egi1mN8akLfuQiSwOp XtrC8YSGyG5ApHj8KOHLAAAABmdpdHBvZAECAwQFBgc= -----END OPENSSH PRIVATE KEY-----"
+
 # See php and required modules
 ALL_VERSIONS="7.2 7.3 7.4 8.0 8.1"
 
@@ -16,3 +18,4 @@ which /home/gitpod/go/bin/MailHog || (echo "MailHog Missing" && exit 1)
 convert --version | grep "^Version" || (echo "convert missing" && exit 1)
 identify --version | grep "^Version" || (echo "identify issing" && exit 1)
 docker --version || (echo "Docker missing" && exit 1)
+tdk ssh-add || (echo "Failed adding private key" && exit 1)
