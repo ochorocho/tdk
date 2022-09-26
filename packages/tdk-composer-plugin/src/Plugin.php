6d3bb09cc98f548c63c5a003e8621d05c9239f8b
ochorocho <jochen.roth@b13.com>
2022-09-26 08:02:02 +0200

    [TASK] TEST Create project

--- packages/tdk-composer-plugin/src/Plugin.php
+++ packages/tdk-composer-plugin/src/Plugin.php
@@ -90,7 +90,7 @@ final class Plugin implements PluginInterface, CapableInterface, EventSubscriber
 
                 $input = new ArrayInput(array('command' => 'tdk:git', 'action' => 'clone'));
                 $this->application->run($input);
-                $event->getComposer()->getEventDispatcher()->dispatchScript('typo3-clone-done', true);
+                // $event->getComposer()->getEventDispatcher()->dispatchScript('typo3-clone-done', true);
 
                 // $this->composerService->requireAllCoreExtensions();
                 $coreExtensions = $this->composerService->getCoreExtensions();
